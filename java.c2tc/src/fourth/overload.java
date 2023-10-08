7e3d9aaa2285bb213e29b7ba10e5822575d76992
swathi-d27 <swathidharma11@gmail.com>
2023-10-08 15:20:02 +0530

    first commit

---
+++ java.c2tc/src/fourth/overload.java
@@ -0,0 +1,15 @@
+package fourth;
+
+public class overload {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		point p=new point();
+		System.out.println(p);
+		point p1=new point(0.2f);
+		System.out.println(p1);
+		point p2=new point(0.3f,0.9f);
+		System.out.println(p2);
+		
+}
+}
---
+++ java.c2tc/src/fourth/point.java
@@ -0,0 +1,43 @@
+package fourth;
+
+public class point {
+	private float x;
+	private float y;
+	
+	public point()
+	{
+		x=0.0f;
+		y=0.0f;
+	}
+	public point(float x)
+	{
+		this.x=x;
+		this.y=x;
+		
+	}
+	public point(float x,float y)
+	{
+		this.x=x;
+		this.y=y;
+		
+	}
+	
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
+	public String toString() {
+		return "point [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+
+}
